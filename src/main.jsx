e94f39aff51dcca1a202ea394bacca9639f07658
felipedasilveira095-bit <felipedasilveira095@gmail.com>
2026-05-16 22:11:50 -0300

    Add main.jsx to render PulseDelivery component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PulseDelivery from "./pulse-delivery";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PulseDelivery />
+  </React.StrictMode>
+);
